f399f13f722bdcc675839fef156943f789ec962e
Karl Jones <karl@unity3d.com>
2016-01-30 21:43:58 +0000

    stuff!!!

---
+++ Assets/Scripts/UI/UIInput.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using Assets.Scripts;
+
+public class UIInput : MonoBehaviour
+{
+	public string playerInput;
+	
+	void Start ()
+	{
+		var plyInput = GameObject.Find(playerInput);
+		if (plyInput == null)
+		{
+			Debug.LogWarning("Could not find " + playerInput) ;
+		}
+		else
+		{
+			var input = plyInput.GetComponent<PlayerComponent>();
+			input.
+		}
+	}
+	
+}
