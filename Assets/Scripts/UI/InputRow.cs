e95ae01bb0699e096897734dbdae7ec700a7446c
Karl Jones <karl@unity3d.com>
2016-01-30 21:58:11 +0000

    errghhh

--- Assets/Scripts/UI/InputRow.cs
+++ Assets/Scripts/UI/InputRow.cs
@@ -1,12 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Diagnostics;
 using UnityEngine.Events;
 using UnityEngine.UI;
+using Input = Assets.Scripts.Player.Input;
 
 public class InputRow : MonoBehaviour
 {
 	public Row row;
 	public UnityEvent onIncorrectInput;
+	public string side = "Bad side/Player Input";
+
+	void Start()
+	{
+		var input = GameObject.Find(side).GetComponent<Input>();
+		input.OnKeyPressed.AddListener(ActivateButton);
+	}
 
 	public void ResetButtons()
 	{
@@ -16,10 +25,13 @@ public class InputRow : MonoBehaviour
 		}
 	}
 
-	public void ActivateButton(string input, int button)
+	public void ActivateButton(int button, string input)
 	{
+		print("ActivateButton");
 		Sprite s = ImageMap.instance.GetSprite(input);
 		row.buttons[button].button.sprite = s;
 		row.buttons[button].buttonSelector.interactable = true;
 	}
+
+
 }
