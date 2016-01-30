df109aafdee51258d46c665cf5a25301dff56542
Karl Jones <karl@unity3d.com>
2016-01-30 20:24:05 +0000

    Setup multi scene loading

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+	public string[] scenes;
+
+	void Start()
+	{
+		foreach (var scn in scenes)
+		{
+			SceneManager.LoadScene(scn);
+		}
+	}
+
+}
