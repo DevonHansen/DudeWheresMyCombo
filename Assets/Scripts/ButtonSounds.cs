4c53eb03daa8d17952fcb216eee97cb33939591a
Karl Jones <karl@unity3d.com>
2016-01-31 14:42:23 +0000

    button sounds

---
+++ Assets/Scripts/ButtonSounds.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonSounds : MonoBehaviour
+{
+	public AudioClip[] clips;
+	int current = 0;
+
+	public void Reset()
+	{
+		current = 0;
+	}
+
+	public void DoButton()
+	{ 
+		AudioSource.PlayClipAtPoint(clips[current++], new Vector3(3, 5, -10), 10);
+	}
+}
