a578d96585f31c36f440e55f6de0a112a2a7e9c7
Karl Jones <karl@unity3d.com>
2016-01-31 14:07:30 +0000

    evil attack

---
+++ Assets/Scripts/ReloadScene.cs
@@ -0,0 +1,34 @@
+﻿namespace Assets.Scripts
+{
+    using System.Collections.Generic;
+
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+
+    public class ReloadScene : MonoBehaviour
+    {
+        [SerializeField]
+        private string m_SceneName;
+
+        [SerializeField]
+        private GameObject m_EndGameObject;
+
+        [SerializeField]
+        private List<Player.Input> m_InputScripts;
+
+        public void ReloadGivenScene()
+        {
+            SceneManager.LoadScene(this.m_SceneName);
+        }
+
+        public void OnEndGame()
+        {
+            foreach (var input in this.m_InputScripts)
+            {
+                input.gameObject.SetActive(false);
+            }
+
+            this.m_EndGameObject.SetActive(true);
+        }
+    }
+}
